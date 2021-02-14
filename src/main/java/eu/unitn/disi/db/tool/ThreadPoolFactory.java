ed8521f9d0b4efc234582c2c876c41c0a221c9d6
Zhaoyang Shao <zhaoshao@amazon.com>
2021-02-14 14:24:31 -0800

    Increa thread pool size

--- src/main/java/eu/unitn/disi/db/tool/ThreadPoolFactory.java
+++ src/main/java/eu/unitn/disi/db/tool/ThreadPoolFactory.java
@@ -5,30 +5,42 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ForkJoinPool;
 
 public class ThreadPoolFactory {
-    private static final ExecutorService WILDCARD_SEARCH_THREAD_POOL = Executors.newFixedThreadPool(8);
-    private static final ExecutorService WILDCARD_THREAD_POOL = Executors.newFixedThreadPool(8);
-    private static final ExecutorService TABLE_COMPUTE_THREAD_POOL = Executors.newFixedThreadPool(8);
-    private static final ExecutorService SEARCH_THREAD_POOL = Executors.newFixedThreadPool(1);
-    private static final ForkJoinPool FORK_JOIN_POOL = new ForkJoinPool(8);
+    private static ExecutorService WILDCARD_SEARCH_THREAD_POOL = null;
+    private static ExecutorService WILDCARD_THREAD_POOL = null;
+    private static ExecutorService TABLE_COMPUTE_THREAD_POOL = null;
+    private static ExecutorService SEARCH_THREAD_POOL = null;
+    private static ForkJoinPool FORK_JOIN_POOL = null;
 
     public static ExecutorService getWildcardThreadPool() {
         return WILDCARD_THREAD_POOL;
     }
 
-    public static ForkJoinPool getForkJoinPool() {
+    public synchronized static ForkJoinPool getForkJoinPool() {
+        if (FORK_JOIN_POOL == null) {
+            FORK_JOIN_POOL = new ForkJoinPool(84);
+        }
         return FORK_JOIN_POOL;
     }
 
-    public static ExecutorService getWildcardSearchThreadPool() {
+    public synchronized static ExecutorService getWildcardSearchThreadPool() {
+        if (WILDCARD_SEARCH_THREAD_POOL == null) {
+            WILDCARD_SEARCH_THREAD_POOL = Executors.newFixedThreadPool(8);
+        }
         return WILDCARD_SEARCH_THREAD_POOL;
     }
 
 
-    public static ExecutorService getTableComputeThreadPool() {
+    public synchronized static ExecutorService getTableComputeThreadPool() {
+        if (TABLE_COMPUTE_THREAD_POOL == null) {
+            TABLE_COMPUTE_THREAD_POOL = Executors.newFixedThreadPool(8);
+        }
         return TABLE_COMPUTE_THREAD_POOL;
     }
 
-    public static ExecutorService getSearchThreadPool() {
+    public synchronized static ExecutorService getSearchThreadPool() {
+        if (SEARCH_THREAD_POOL == null) {
+            SEARCH_THREAD_POOL = Executors.newFixedThreadPool(1);
+        }
         return SEARCH_THREAD_POOL;
     }
 
