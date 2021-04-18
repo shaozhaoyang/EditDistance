0d32c4576d0f636f2377c676cffeca83eab9142e
Zhaoyang Shao <zhaoshao@amazon.com>
2021-04-18 01:22:38 -0700

    Generate graphs with different degrees

--- src/main/java/eu/unitn/disi/db/tool/GraphGenerator.java
+++ src/main/java/eu/unitn/disi/db/tool/GraphGenerator.java
@@ -30,10 +30,14 @@ public class GraphGenerator {
 
     private static final long MAX_NODE_COUNT = 10000;
     private static final int MAX_DEGREE = 5;
+    private static final int MIN_DEGREE_TO_BFS = 5;
     private long nodeCount = 0;
 
     public GraphGenerator(String fileName) throws IOException {
-        process(fileName);
+        final Map<Long, Queue<Edge>> edgeMap = read(fileName);
+        for (int i = 5; i <= 25; i += 5) {
+            bfs(edgeMap, i);
+        }
     }
 
     /**
@@ -43,18 +47,63 @@ public class GraphGenerator {
     public static void main(String[] args) throws IOException {
 //		String a = "<http://rdf.freebase.com/ns/award.award_winner>";
 //		System.out.println(a.split("/")[a.split("/").length-1]);
-        GraphGenerator rf = new GraphGenerator("freebase-sout.graph");
+        GraphGenerator rf = new GraphGenerator("graph/100000nodes-sout.graph");
 //		String a = "<http://rdf.freebase.com/ns/american_football.football_player.footballdb_id>    <http://www.w3.org/2000/01/rdf-schema#label>    \"footballdb ID\"@en      .";
 //		System.out.println(a.split(" ")[2]);
     }
 
-    private void process(String fileName) throws IOException {
+    private Map<Long, Queue<Edge>> read(String fileName) throws IOException {
+        FileInputStream fin = new FileInputStream(fileName);
+        InputStreamReader xover = new InputStreamReader(fin);
+        BufferedReader is = new BufferedReader(xover);
+        String line;
+        // Now read lines of text: the BufferedReader puts them in lines,
+        // the InputStreamReader does Unicode conversion, and the
+        // GZipInputStream "gunzip"s the data from the FileInputStream.
+
+        Map<Long, Queue<Edge>> edgeMap = new HashMap<>();
+        long crtNodeId;
+        try {
+            int count = 0;
+
+            while ((line = is.readLine()) != null) {
+                String[] words = line.split(" ");
+                if (words.length < 3) {
+                    continue;
+                }
+                Long subjectId = Long.parseLong(words[0]);
+                Long objectId = Long.parseLong(words[1]);
+                Long predicateId = Long.parseLong(words[2]);
+                crtNodeId = subjectId;
+                Queue<Edge> edges = edgeMap.getOrDefault(crtNodeId, new PriorityQueue<>(
+                        Comparator.comparingLong(Edge::getDestination)));
+
+                edges.add(new Edge(subjectId, objectId, predicateId));
+
+                edgeMap.put(crtNodeId, edges);
+
+                count++;
+                if (count % 100000 == 0) {
+                    System.out.println("Processed " + count);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (is != null) {
+                is.close();
+            }
+        }
+        return edgeMap;
+    }
+
+    private void process(String fileName, int maxDegree) throws IOException {
         // Since there are 4 constructor calls here, I wrote them out in full.
         // In real life you would probably nest these constructor calls.
         FileInputStream fin = new FileInputStream(fileName);
         InputStreamReader xover = new InputStreamReader(fin);
         BufferedReader is = new BufferedReader(xover);
-        BufferedWriter bw = new BufferedWriter(new FileWriter( + MAX_NODE_COUNT + "nodes-sout" + "-d" + MAX_DEGREE +
+        BufferedWriter bw = new BufferedWriter(new FileWriter( + MAX_NODE_COUNT + "nodes-sout" + "-d" + maxDegree +
                 ".graph",
                 false));
         String line;
@@ -98,15 +147,16 @@ public class GraphGenerator {
                                     + crtNodeId + "; queue "
                                     + "peek " + queue.peek() + " queue size:" + queue.size());
                 }
-                if (subjectId.equals(crtNodeId)) {
-                    continue;
-                } else if (crtNodeId == queue.peek()) {
-                    bfs(bw, edgeMap, queue, visited, allNodes, countMap);
-                } else if (crtNodeId > queue.peek()) {
-                    bfs(bw, edgeMap, queue, visited, allNodes, countMap);
-                }
-                crtNodeId = subjectId;
+//                if (subjectId.equals(crtNodeId)) {
+//                    continue;
+//                } else if (crtNodeId == queue.peek()) {
+//                    bfs(bw, edgeMap, queue, visited, allNodes, countMap);
+//                } else if (crtNodeId > queue.peek()) {
+//                    bfs(bw, edgeMap, queue, visited, allNodes, countMap);
+//                }
+//                crtNodeId = subjectId;
             }
+            bfs(bw, edgeMap, queue, visited, allNodes, countMap);
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
@@ -119,6 +169,85 @@ public class GraphGenerator {
                 bw.close();
             }
         }
+    }
+
+    private void bfs(final Map<Long, Queue<Edge>> edgeMap, final int maxDegree) throws IOException {
+        BufferedWriter writer = new BufferedWriter(new FileWriter( + MAX_NODE_COUNT + "nodes-sout" + "-d" + maxDegree +
+                ".graph",
+                false));
+
+        Set<Long> allNodes = new HashSet<>();
+        Set<Long> visited = new HashSet<>();
+        Queue<Long> toVisit = new LinkedList<>();
+
+
+        long maxDegreeNode = edgeMap.entrySet().iterator().next().getKey();
+        long crtMaxDegree = 0;
+        for (Map.Entry<Long, Queue<Edge>> entry : edgeMap.entrySet()) {
+            if (entry.getValue().size() > crtMaxDegree) {
+                maxDegreeNode = entry.getKey();
+                crtMaxDegree = entry.getValue().size();
+            }
+        }
+        toVisit.add(maxDegreeNode);
+
+        final Map<Long, Integer> countMap = new HashMap<>();
+
+        while (!toVisit.isEmpty() && allNodes.size() < MAX_NODE_COUNT) {
+            if (edgeMap.get(toVisit.peek()) != null) {
+                Long crtNode = toVisit.poll();
+                if (visited.contains(crtNode)) {
+                    continue;
+                }
+                Queue<Edge> edges = edgeMap.get(crtNode);
+                List<Edge> usedEdges = new LinkedList<>();
+                int count = 0;
+                for (Edge edge : edges) {
+                    int sourceCount = countMap.getOrDefault(edge.getSource(), 0);
+                    sourceCount++;
+
+                    int dstCount = countMap.getOrDefault(edge.getDestination(), 0);
+                    dstCount++;
+
+
+                    if (edgeMap.get(edge.getDestination()) != null
+                            && sourceCount <= MAX_DEGREE && dstCount <= MAX_DEGREE) {
+
+
+                        usedEdges.add(edge);
+
+                        countMap.put(edge.getSource(), sourceCount);
+                        countMap.put(edge.getDestination(), dstCount);
+                        count++;
+                        if (count > MAX_DEGREE) {
+                            break;
+                        }
+                    }
+                }
+                List<Long> nodes = usedEdges.stream()
+                        .map(Edge::getDestination)
+                        .filter(node -> !visited.contains(node))
+                        .collect(Collectors.toList());
+                usedEdges.stream()
+                        .map(Edge::getDestination)
+                        .forEach(allNodes::add);
+                usedEdges.stream()
+                        .map(Edge::getSource)
+                        .forEach(allNodes::add);
+                write(writer, usedEdges);
+                toVisit.addAll(nodes);
+//                }
+                visited.add(crtNode);
+                edgeMap.remove(crtNode);
+                edges = null;
+                nodes = null;
+            } else {
+                toVisit.poll();
+                break;
+            }
+        }
+        writer.flush();
+        writer.close();
     }
 
     private void bfs(final BufferedWriter writer, final Map<Long, Queue<Edge>> edgeMap, final Queue<Long> queue,
@@ -140,11 +269,11 @@ public class GraphGenerator {
                     dstCount++;
 
 
-                    countMap.put(edge.getSource(), sourceCount);
-                    countMap.put(edge.getDestination(), dstCount);
-
-                    if (sourceCount <= MAX_DEGREE && dstCount <= MAX_DEGREE) {
+                    if (edgeMap.get(edge.getDestination()).size() >= MIN_DEGREE_TO_BFS && sourceCount <= MAX_DEGREE && dstCount <= MAX_DEGREE) {
                         usedEdges.add(edge);
+
+                        countMap.put(edge.getSource(), sourceCount);
+                        countMap.put(edge.getDestination(), dstCount);
                         count++;
                         if (count > MAX_DEGREE) {
                             break;
