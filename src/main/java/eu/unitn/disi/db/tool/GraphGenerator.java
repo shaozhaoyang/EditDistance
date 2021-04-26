d8ff061e884f4d14a3e9b731f128d3eb0a250270
Zhaoyang Shao <zhaoshao@amazon.com>
2021-04-26 00:09:15 -0700

    Generate avg graph

--- src/main/java/eu/unitn/disi/db/tool/GraphGenerator.java
+++ src/main/java/eu/unitn/disi/db/tool/GraphGenerator.java
@@ -1,6 +1,7 @@
 package eu.unitn.disi.db.tool;
 
 import eu.unitn.disi.db.grava.graphs.Edge;
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileInputStream;
@@ -10,6 +11,7 @@ import java.io.InputStreamReader;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -35,9 +37,8 @@ public class GraphGenerator {
     private long nodeCount = 0;
 
     public GraphGenerator(String fileName) throws IOException {
-        final Map<Long, List<Edge>> edgeMap = read(fileName);
-        final long totalDegree = totalDegree(edgeMap);
-        for (int i = 5; i <= 25; i += 5) {
+        final Map<Long, Set<Edge>> edgeMap = read(fileName);
+        for (int i = 25; i <= 25; i += 5) {
             bfs(edgeMap, i);
         }
     }
@@ -49,12 +50,13 @@ public class GraphGenerator {
     public static void main(String[] args) throws IOException {
 //		String a = "<http://rdf.freebase.com/ns/award.award_winner>";
 //		System.out.println(a.split("/")[a.split("/").length-1]);
-        GraphGenerator rf = new GraphGenerator("graph/100000nodes-sout.graph");
-//		String a = "<http://rdf.freebase.com/ns/american_football.football_player.footballdb_id>    <http://www.w3.org/2000/01/rdf-schema#label>    \"footballdb ID\"@en      .";
+        GraphGenerator rf = new GraphGenerator("freebase-sout.graph");
+//		String a = "<http://rdf.freebase.com/ns/american_football.football_player.footballdb_id>    <http://www.w3
+//		.org/2000/01/rdf-schema#label>    \"footballdb ID\"@en      .";
 //		System.out.println(a.split(" ")[2]);
     }
 
-    private Map<Long, List<Edge>> read(String fileName) throws IOException {
+    private Map<Long, Set<Edge>> read(String fileName) throws IOException {
         FileInputStream fin = new FileInputStream(fileName);
         InputStreamReader xover = new InputStreamReader(fin);
         BufferedReader is = new BufferedReader(xover);
@@ -63,7 +65,7 @@ public class GraphGenerator {
         // the InputStreamReader does Unicode conversion, and the
         // GZipInputStream "gunzip"s the data from the FileInputStream.
 
-        Map<Long, List<Edge>> edgeMap = new HashMap<>();
+        Map<Long, Set<Edge>> edgeMap = new HashMap<>();
         long crtNodeId;
         try {
             int count = 0;
@@ -77,7 +79,7 @@ public class GraphGenerator {
                 Long objectId = Long.parseLong(words[1]);
                 Long predicateId = Long.parseLong(words[2]);
                 crtNodeId = subjectId;
-                List<Edge> edges = edgeMap.getOrDefault(crtNodeId, new LinkedList<>());
+                Set<Edge> edges = edgeMap.getOrDefault(crtNodeId, new HashSet<>());
 
                 edges.add(new Edge(subjectId, objectId, predicateId));
 
@@ -95,112 +97,138 @@ public class GraphGenerator {
                 is.close();
             }
         }
+        int count = 0;
+        for (Map.Entry<Long, Set<Edge>> entry : edgeMap.entrySet()) {
+            count += entry.getValue().size();
+        }
+        System.out.println(count);
+        System.out.println(edgeMap.keySet().size());
         return edgeMap;
     }
 
-    private long totalDegree(Map<Long, List<Edge>> edgeMap) {
+    private long totalDegree(Map<Long, Set<Edge>> edgeMap) {
         long totalDegree = 0;
-        for (Map.Entry<Long, List<Edge>> entry: edgeMap.entrySet()) {
+        for (Map.Entry<Long, Set<Edge>> entry : edgeMap.entrySet()) {
             totalDegree += entry.getValue().size() * 2L;
         }
         return totalDegree;
     }
 
-    private void bfs(final Map<Long, List<Edge>> edgeMap, final int maxDegree) throws IOException {
-        BufferedWriter writer = new BufferedWriter(new FileWriter( + MAX_NODE_COUNT + "nodes-sout" + "-d" + maxDegree +
+    private void bfs(final Map<Long, Set<Edge>> edgeMap, final int maxDegree) throws IOException {
+        BufferedWriter writer = new BufferedWriter(new FileWriter(+MAX_NODE_COUNT + "nodes-sout" + "-d" + maxDegree +
                 ".graph",
                 false));
 
         Set<Long> allNodes = new HashSet<>();
         Set<Long> visited = new HashSet<>();
         Queue<Long> toVisit = new LinkedList<>();
-        Queue<Long> moreCandidates = new LinkedList<>();
+        Map<Long, Set<Edge>> highDegreeNodes = new HashMap<>();
 
-        long minDegreeNode = edgeMap.entrySet().iterator().next().getKey();
-        long minDegree = Integer.MAX_VALUE;
-        for (Map.Entry<Long, List<Edge>> entry : edgeMap.entrySet()) {
-            if (entry.getValue().size() < minDegree) {
-                minDegreeNode = entry.getKey();
-                minDegree = entry.getValue().size();
+        long maxDegreeNode = edgeMap.entrySet().iterator().next().getKey();
+        long crtMaxDegree = 0;
+        for (Map.Entry<Long, Set<Edge>> entry : edgeMap.entrySet()) {
+            if (entry.getValue().size() > crtMaxDegree) {
+                maxDegreeNode = entry.getKey();
+                crtMaxDegree = entry.getValue().size();
             }
         }
-        toVisit.add(minDegreeNode);
+        toVisit.add(maxDegreeNode);
 
         final Map<Long, Integer> countMap = new HashMap<>();
         long crtDegree = 0;
 
-        while ((!toVisit.isEmpty() || !moreCandidates.isEmpty()) && allNodes.size() < MAX_NODE_COUNT) {
-            Long crtNode = null;
-            if (!toVisit.isEmpty()) {
-                crtNode = toVisit.poll();
-            } else {
-                crtNode = moreCandidates.poll();
-            }
-
-            if (edgeMap.get(crtNode) != null) {
-                if (visited.contains(crtNode)) {
-                    continue;
-                }
-                List<Edge> edges = edgeMap.get(crtNode);
-                Queue<Edge> sortedEdges = new PriorityQueue<>(new Comparator<Edge>() {
-                    @Override
-                    public int compare(final Edge o1, final Edge o2) {
-                        int d1 = Optional.ofNullable(edgeMap.get(o1.getDestination())).map(List::size).orElse(0);
-                        int d2 = Optional.ofNullable(edgeMap.get(o2.getDestination())).map(List::size).orElse(0);
-                        return d2 - d1;
+        while (allNodes.size() < MAX_NODE_COUNT) {
+            do {
+                Long crtNode = toVisit.poll();
+                if (edgeMap.get(crtNode) != null) {
+                    if (visited.contains(crtNode)) {
+                        continue;
                     }
-                }
-                );
-                sortedEdges.addAll(edges);
+                    Set<Edge> edges = edgeMap.get(crtNode);
 
-                List<Edge> usedEdges = new LinkedList<>();
-                int count = 0;
-                for (Edge edge : sortedEdges) {
-                    int sourceCount = countMap.getOrDefault(edge.getSource(), 0);
-                    sourceCount++;
+                    Queue<Edge> sortedEdges = new PriorityQueue<>(new Comparator<Edge>() {
+                        @Override
+                        public int compare(final Edge o1, final Edge o2) {
+                            int d1 = Optional.ofNullable(edgeMap.get(o1.getDestination())).map(Set::size).orElse(0);
+                            int d2 = Optional.ofNullable(edgeMap.get(o2.getDestination())).map(Set::size).orElse(0);
+                            return d2 - d1;
+                        }
+                    }
+                    );
 
-                    int dstCount = countMap.getOrDefault(edge.getDestination(), 0);
-                    dstCount++;
+                    sortedEdges.addAll(edges);
+
+                    List<Edge> usedEdges = new LinkedList<>();
+                    int count = 0;
+                    visited.add(crtNode);
+                    while (!sortedEdges.isEmpty()) {
+                        Edge edge = sortedEdges.poll();
+                        int sourceCount = countMap.getOrDefault(edge.getSource(), 0);
+                        sourceCount++;
+
+                        int dstCount = countMap.getOrDefault(edge.getDestination(), 0);
+                        dstCount++;
 
 
-                    if (crtDegree/(allNodes.size() + 1) <= MAX_DEGREE) {
-                        usedEdges.add(edge);
+                        if ((crtDegree / visited.size()) <= maxDegree) {
+                            usedEdges.add(edge);
 
-                        countMap.put(edge.getSource(), sourceCount);
-                        countMap.put(edge.getDestination(), dstCount);
-                        count++;
-                        crtDegree += 2;
-                        if (count > MAX_DEGREE) {
+                            countMap.put(edge.getSource(), sourceCount);
+                            countMap.put(edge.getDestination(), dstCount);
+                            count++;
+                            crtDegree += 1;
+                        } else {
                             break;
                         }
+                    }
+                    if (!sortedEdges.isEmpty()) {
+                        Set<Edge> restEdges = new HashSet<>();
+                        restEdges.addAll(sortedEdges);
+                        highDegreeNodes.put(crtNode, restEdges);
+                    }
+                    List<Long> nodes = usedEdges.stream()
+                            .map(Edge::getDestination)
+                            .filter(node -> !visited.contains(node))
+                            .collect(Collectors.toList());
+                    usedEdges.stream()
+                            .map(Edge::getDestination)
+                            .forEach(allNodes::add);
+                    usedEdges.stream()
+                            .map(Edge::getSource)
+                            .forEach(allNodes::add);
+                    write(writer, usedEdges);
+                    toVisit.addAll(nodes);
+//                }
+                    edgeMap.remove(crtNode);
+                    edges = null;
+                    nodes = null;
+                }
+            } while ((crtDegree / visited.size()) > maxDegree);
+
+            for (Map.Entry<Long, Set<Edge>> entry: highDegreeNodes.entrySet()) {
+                Iterator<Edge> iterator = entry.getValue().iterator();
+                List<Edge> usedEdges = new LinkedList<>();
+                while (iterator.hasNext()) {
+                    if ((crtDegree / visited.size()) < maxDegree || allNodes.size() < MAX_NODE_COUNT) {
+                        Edge element = iterator.next();
+                        usedEdges.add(element);
+                        toVisit.add(element.getDestination());
+                        allNodes.add(element.getDestination());
+                        crtDegree += 1;
+                        iterator.remove();
                     } else {
-                        if (!visited.contains(edge.getDestination())) {
-                            moreCandidates.add(edge.getDestination());
-                        }
+                        break;
                     }
                 }
-                List<Long> nodes = usedEdges.stream()
-                        .map(Edge::getDestination)
-                        .filter(node -> !visited.contains(node))
-                        .collect(Collectors.toList());
-                usedEdges.stream()
-                        .map(Edge::getDestination)
-                        .forEach(allNodes::add);
-                usedEdges.stream()
-                        .map(Edge::getSource)
-                        .forEach(allNodes::add);
                 write(writer, usedEdges);
-                toVisit.addAll(nodes);
-                long crtAvg = crtDegree/allNodes.size();
-//                }
-                visited.add(crtNode);
-                edgeMap.remove(crtNode);
-                edges = null;
-                nodes = null;
+                if ((crtDegree / allNodes.size()) > maxDegree || allNodes.size() < MAX_NODE_COUNT) {
+                    break;
+                }
             }
+            highDegreeNodes.entrySet().removeIf(e -> e.getValue().isEmpty());
         }
 //        System.out.println(allNodes.size());
-        System.out.println("avg degree:" + crtDegree/(allNodes.size()));
+        System.out.println("avg degree:" + crtDegree / (visited.size()));
         writer.flush();
         writer.close();
     }
